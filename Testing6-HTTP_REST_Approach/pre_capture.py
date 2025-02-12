cdfa6d7bfaf835f99f04b66be8850d3c02e30813
Ivan Masinde <u20473258@tuks.co.za>
2025-02-12 11:43:35 +0200

    updated pre-capture script for raspberry pis.

--- Testing6-HTTP_REST_Approach/pre_capture.py
+++ Testing6-HTTP_REST_Approach/pre_capture.py
@@ -1,27 +1,24 @@
 import os
 import shutil
 import sys
-import pyrealsense2 as rs
 
 # Deletes all the camera data directories
-def delete_directories(serials):
-    for i in serials:
-        if os.path.exists(f"camera_{i}"):
-            shutil.rmtree(f"camera_{i}")
+def delete_directories(serial):
+    if os.path.exists(f"camera_{serial}"):
+        shutil.rmtree(f"camera_{serial}")
 
 # Creates new directories for the camera data       
-def create_directories(serials):
-    for i in serials:
-        os.makedirs(f"camera_{i}", exist_ok=True)
-        os.makedirs(f"camera_{i}/colour", exist_ok=True)
-        os.makedirs(f"camera_{i}/depth", exist_ok=True)
-        os.makedirs(f"camera_{i}/depth_colourised", exist_ok=True)
-        os.makedirs(f"camera_{i}/colour_metadata", exist_ok=True)
-        os.makedirs(f"camera_{i}/depth_metadata", exist_ok=True)
+def create_directories(serial):
+    os.makedirs(f"camera_{serial}", exist_ok=True)
+    os.makedirs(f"camera_{serial}/colour", exist_ok=True)
+    os.makedirs(f"camera_{serial}/depth", exist_ok=True)
+    os.makedirs(f"camera_{serial}/depth_colourised", exist_ok=True)
+    os.makedirs(f"camera_{serial}/colour_metadata", exist_ok=True)
+    os.makedirs(f"camera_{serial}/depth_metadata", exist_ok=True)
 
 
 if __name__ == "__main__":
-    serial_numbers = [138322250306, 141322252882]
+    serial_numbers = [138322250306]
     
     delete_directories(serial_numbers)
     create_directories(serial_numbers)
