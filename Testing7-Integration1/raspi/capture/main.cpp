b1dc9533fe866acc0a40f119f99133c9e0404193
Ivan Masinde <u20473258@tuks.co.za>
2025-02-14 10:55:24 +0200

    Modified capture duration.

--- Testing7-Integration1/raspi/capture/main.cpp
+++ Testing7-Integration1/raspi/capture/main.cpp
@@ -2,7 +2,6 @@
 // Copyright(c) 2017 Intel Corporation. All Rights Reserved.
 
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
-#include <librealsense2-net/rs_net.hpp>
 
 // 3rd party header for writing png files
 #define STB_IMAGE_WRITE_IMPLEMENTATION
@@ -12,8 +11,6 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
-#include <map>
-#include <vector>
 
 
 void metadata_to_csv(const rs2::frame& frm, const std::string& filename)
@@ -37,14 +34,14 @@ void metadata_to_csv(const rs2::frame& frm, const std::string& filename)
     csv.close();
 }
 
-void save_frame_depth_data(const std::string& serial_number, rs2::frame frame)
+void save_frame_depth_data(const std::string& pi_name, rs2::frame frame)
 {
     rs2::depth_frame depth = frame.as<rs2::depth_frame>();
     if (auto image = frame.as<rs2::video_frame>())
     {
         std::ofstream myfile;
         std::stringstream filename;
-        filename << "camera_" << serial_number << "/depth/" << frame.get_frame_number() << ".csv";
+        filename << "depth/" << pi_name << "_depth_" << frame.get_frame_number() << ".csv";
         myfile.open(filename.str());
         myfile << std::setprecision(2);
 
@@ -58,29 +55,29 @@ void save_frame_depth_data(const std::string& serial_number, rs2::frame frame)
         }
         std::cout << "Saved " << filename.str() << std::endl;
         myfile.close();
-        
+
         // Record per-frame metadata for UVC streams
         std::stringstream csv_file;
-        csv_file << "camera_" << serial_number << "/depth_metadata/" << frame.get_frame_number() << ".csv";
+        csv_file << "depth_metadata/" << pi_name << "_depth_metadata_" << frame.get_frame_number() << ".csv";
         metadata_to_csv(image, csv_file.str());
     }
 }
 
-void save_frame_color_data(const std::string& serial_number, rs2::frame frame)
+void save_frame_color_data(const std::string& pi_name, rs2::frame frame)
 {
     // We can only save video frames as pngs, so we skip the rest
     if (auto image = frame.as<rs2::video_frame>())
     {
         // Write images to disk
         std::stringstream png_file;
-        png_file << "camera_" << serial_number << "/colour/" << frame.get_frame_number() << ".png";
+        png_file << "colour/" << pi_name << "_colour_" << frame.get_frame_number() << ".png";
         stbi_write_png(png_file.str().c_str(), image.get_width(), image.get_height(),
                        image.get_bytes_per_pixel(), image.get_data(), image.get_stride_in_bytes());
         std::cout << "Saved " << png_file.str() << std::endl;
 
         // Record per-frame metadata for UVC streams
         std::stringstream csv_file;
-        csv_file << "camera_" << serial_number << "/colour_metadata/" << frame.get_frame_number() << ".csv";
+        csv_file << "colour_metadata/" << pi_name << "_colour_metadata_" << frame.get_frame_number() << ".csv";
         metadata_to_csv(image, csv_file.str());
     }
 
@@ -90,56 +87,29 @@ void save_frame_color_data(const std::string& serial_number, rs2::frame frame)
 // capture depth and color video streams and render them to the screen
 int main(int argc, char * argv[]) try
 {
+    rs2::colorizer color_map;
+
+    rs2::pipeline pipe;
+    pipe.start();
+
     // Create librealsense context for managing devices
     rs2::context ctx;
 
-    // Create network devices and add them to the context
-    rs2::net_device raspi1("192.168.249.151"); raspi1.add_to(ctx);
-    rs2::net_device raspi2("192.168.249.150"); raspi2.add_to(ctx);
+    // Get the serial number of the connected device. There should only be one connected
+    std::string raspi_name = "raspi1";
 
-    // Declare map from device serial number to colorizer (utility class to convert depth data RGB colorspace)
-    std::map<std::string, rs2::colorizer> colorizers;
+    // Capture 30 frames to give autoexposure, etc. a chance to settle
+    for (auto i = 0; i < 30; ++i) pipe.wait_for_frames();
 
-    // Create pipelines for the cameras
-    std::vector<rs2::pipeline> pipelines;
-
-    // Capture serial numbers before opening streaming
-    std::vector<std::string> serials = {"138322250306", "141322252882"};
-    int num_cameras = 2;
-
-    // Start a streaming pipe per each connected device
-    for (auto&& serial : serials)
+    for (auto i = 0; i < 100; ++i) // Application still alive?
     {
-        rs2::pipeline pipe(ctx);
-        rs2::config cfg;
-        cfg.enable_device(serial);
-        cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
-        cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_RGB8 , 30);
-        pipe.start(cfg);
-        pipelines.emplace_back(pipe);
-        // Map from each device's serial number to a different colorizer
-        colorizers[serial] = rs2::colorizer();
-    }
+        rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
 
-    // Main app loop
-    while (true) {
-        rs2::frame depth, color;
+        rs2::frame depth = data.get_depth_frame(); // Find the depth data
+        rs2::frame color = data.get_color_frame(); // Find the color data
 
-        // Collect and save the new frames from all the connected devices
-        for (int i = 0; i < num_cameras; i++) {
-            // Wait for next set of frames from the camera
-            rs2::frameset data = pipelines[i].wait_for_frames();
-
-            // Find the depth data
-            depth = data.get_depth_frame();
-
-            // Find the color data
-            color = data.get_color_frame();
-
-            // Save the data
-            save_frame_depth_data(serials[i], depth);
-            save_frame_color_data(serials[i], color);
-        }
+        save_frame_depth_data(raspi_name, depth);
+        save_frame_color_data(raspi_name, color);
     }
 
     return EXIT_SUCCESS;
@@ -154,3 +124,5 @@ catch (const std::exception& e)
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
 }
+
+
