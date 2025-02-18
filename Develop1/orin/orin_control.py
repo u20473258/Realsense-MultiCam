0fd68c667879ca858885d7b4c0d48d9c23fd96d5
Ivan Masinde <u20473258@tuks.co.za>
2025-02-18 15:04:55 +0200

    Added comments, more capture commands, and tidied up Orin controller.

--- Develop1/orin/orin_control.py
+++ Develop1/orin/orin_control.py
@@ -1,18 +1,109 @@
 import os
 import shutil
 import socket
-import threading
 import time
 from flask import Flask, request, jsonify
 import numpy as np
 import csv
 import cv2
 
+# Initialise flask server
 app = Flask(__name__)      
      
 
-################### CAPTURE AND TRANSFER STATE ###################### 
+""" Uses UDP to broadcast commands to Raspberry Pi 5s in the network """
+def send_command_to_raspis(command, capture_duration):
+    # Broadcast address to send to all devices in the subnet
+    BROADCAST_IP = "192.168.249.255"
+    
+    # Port to broadcast on
+    PORT = 5005
 
+    # Create a UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    # Store the different capture commands
+    capture_commands = ["CAPTURE_1s",
+                "CAPTURE_2s",
+                "CAPTURE_5s",
+                "CAPTURE_10s",
+                "CAPTURE_15s",
+                "CAPTURE_20s",
+                "CAPTURE_25s",
+                "CAPTURE_30s",
+                "CAPTURE_60s",
+                "CAPTURE_100s"]
+    
+    try:
+        if command == 'C':
+            if capture_duration == 1:
+                sock.sendto(capture_commands[0].encode(), (BROADCAST_IP, PORT))
+                print(f"Broadcast message sent: {capture_commands[0]}")
+                time.sleep(5)
+                
+            elif capture_duration == 2:
+                sock.sendto(capture_commands[1].encode(), (BROADCAST_IP, PORT))
+                print(f"Broadcast message sent: {capture_commands[1]}")
+                time.sleep(5)
+                
+            elif capture_duration == 5:
+                sock.sendto(capture_commands[2].encode(), (BROADCAST_IP, PORT))
+                print(f"Broadcast message sent: {capture_commands[2]}")
+                time.sleep(5)
+                
+            elif capture_duration == 10:
+                sock.sendto(capture_commands[3].encode(), (BROADCAST_IP, PORT))
+                print(f"Broadcast message sent: {capture_commands[3]}")
+                time.sleep(5)
+                
+            elif capture_duration == 15:
+                sock.sendto(capture_commands[4].encode(), (BROADCAST_IP, PORT))
+                print(f"Broadcast message sent: {capture_commands[4]}")
+                time.sleep(5)
+                
+            elif capture_duration == 20:
+                sock.sendto(capture_commands[5].encode(), (BROADCAST_IP, PORT))
+                print(f"Broadcast message sent: {capture_commands[5]}")
+                time.sleep(5)
+                
+            elif capture_duration == 25:
+                sock.sendto(capture_commands[6].encode(), (BROADCAST_IP, PORT))
+                print(f"Broadcast message sent: {capture_commands[6]}")
+                time.sleep(5)
+                
+            elif capture_duration == 30:
+                sock.sendto(capture_commands[7].encode(), (BROADCAST_IP, PORT))
+                print(f"Broadcast message sent: {capture_commands[7]}")
+                time.sleep(5)
+                
+            elif capture_duration == 60:
+                sock.sendto(capture_commands[8].encode(), (BROADCAST_IP, PORT))
+                print(f"Broadcast message sent: {capture_commands[8]}")
+                time.sleep(5)
+                
+            elif capture_duration == 100:
+                sock.sendto(capture_commands[9].encode(), (BROADCAST_IP, PORT))
+                print(f"Broadcast message sent: {capture_commands[9]}")
+                time.sleep(5)
+                
+            else:
+                print("Incorrect capture duration received, terminating program...")
+                exit(1)
+            
+        elif command == 'R':
+            reboot_command = "REBOOT"
+            sock.sendto(reboot_command.encode(), (BROADCAST_IP, PORT))
+            print(f"Broadcast message sent: {reboot_command}")
+            time.sleep(5)
+            
+    except KeyboardInterrupt:
+        print("Broadcasting stopped.")
+    finally:
+        sock.close()
+
+
+""" Runs the flask server """
 @app.route('/uploads', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
@@ -28,66 +119,25 @@ def upload_file():
     
     return jsonify({"message": f"File {file.filename} saved at {file_path}"})
 
-def shutdown_after_duration(duration):
-    """Run a timer and then shut down the server after the given duration."""
-    print(f"[INFO] Server will run for {duration} seconds.")
-    time.sleep(duration)
-    print("\n[INFO] Time's up! Shutting down the server...")
-    os._exit(0)  # Forcefully exits the process
 
-# Run capture script for 30s    
-def receive_data():
+""" Receives the files sent from the raspberry pi 5s. A flask server is created and the program
+then waits for files from the pis. The server is terminated by inputting: Ctrl + C, into the 
+terminal. """
+def receive_files_from_pis():
+    # Delete previous uploads folder and then create a new one
     UPLOAD_FOLDER = './uploads'
-    # Checkes if directories exist
     if os.path.exists(f"uploads"):
         shutil.rmtree(UPLOAD_FOLDER)
-    os.makedirs(UPLOAD_FOLDER, exist_ok=True)  # Create the uploads folder if it doesn't exist
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    
+    # Tell which folder to use
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     
-    # duration = 100  # Duration in seconds (e.g., 5 minutes = 300 seconds)
-    # timer_thread = threading.Thread(target=shutdown_after_duration, args=(duration,))
-    # timer_thread.start()
+    # Start Flask server
     app.run(host='0.0.0.0', port=5000)
 
 
-################### WAIT STATE ###################### 
-
-# Wait for user to start program and then broadcast to raspberry pis
-def wait_for_capture():
-    BROADCAST_IP = "192.168.249.255"  # Broadcast address to send to all devices in the subnet
-    PORT = 5005                       # Port to broadcast on
-
-    # Create a UDP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-    commands = ["CAPTURE_30", "REBOOT"]  # Different command types
-    
-    standby = True
-    while (standby):
-        start = input("Press Y to start capture...\n")
-        if start == 'Y':
-            standby = False
-    command = input("Press 1 to capture and 2 to reboot raspberry pis...\n")
-    try:
-        if command == '1':
-            sock.sendto(commands[0].encode(), (BROADCAST_IP, PORT))
-            print(f"Broadcast message sent: {commands[0]}")
-            time.sleep(5)
-        elif command == '2':
-            sock.sendto(commands[1].encode(), (BROADCAST_IP, PORT))
-            print(f"Broadcast message sent: {commands[1]}")
-            time.sleep(5)
-    except KeyboardInterrupt:
-        print("Broadcasting stopped.")
-    finally:
-        sock.close()
-
-
-################### PROCESSING STATE ###################### 
-
-# Convert the depth image .csv to depth image pngs
+""" Perform a basic processing step: convert depth.csv files into pngs and save them """
 def convert_csv_to_depth(raspis):
     image_sets = []
     for i in raspberrys:
@@ -121,16 +171,33 @@ def convert_csv_to_depth(raspis):
 
         # Apply a colour map to the depth image
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=25.5), cv2.COLORMAP_JET)
+        
         # Save depth image
         depth_filename = relative_path + raspis[x] + "_depth_image_" + str(image_sets[x]) + ".png"
         cv2.imwrite(depth_filename, depth_colormap)
+
 
 if __name__ == "__main__":
     
     raspberrys = ["raspi1", "raspi2"]
     
     while(True):
-        wait_for_capture()
-        receive_data()
+        # Promt user to get command
+        print("Please select one of the following commands to proceed:\n")
+        print("C -> Begin Capture\n")
+        print("R -> Reboot Raspberry Pi 5s\n")
+        command = input("Enter command")
+        
+        if command == 'C':
+            print("How long, in seconds, should the cameras capture?\n")
+            capture_duration = int(input("Choose from the following: 1, 2, 5, 10, 15, 20, 25, 30, 60, 100"))
+            send_command_to_raspis(command, capture_duration)
+        elif command == 'R':
+            send_command_to_raspis(command, -1)
+        else:
+            print("Incorrect command received, terminating program...")
+            exit(1)
+            
+        receive_files_from_pis()
         convert_csv_to_depth(raspberrys)
 
