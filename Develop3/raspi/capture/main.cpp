af38148d9f836bd9cbdb37dce1ee8d57b2a7d108
Ivan Masinde <u20473258@tuks.co.za>
2025-07-29 19:45:29 +0200

    Change colour stream resolution to closely match that of the decimated depth frame.

--- Develop3/raspi/capture/main.cpp
+++ Develop3/raspi/capture/main.cpp
@@ -119,7 +119,7 @@ int main(int argc, char * argv[]) try
     // Congifure the streaming configurations
     rs2::config cfg;
     cfg.enable_stream(RS2_STREAM_DEPTH, 1280, 720, RS2_FORMAT_Z16, 15);
-    cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_RGB8, 15);
+    cfg.enable_stream(RS2_STREAM_COLOR, 424, 240, RS2_FORMAT_RGB8, 15);
 
     // Create pipe and start it
     rs2::pipeline pipe;
